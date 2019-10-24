d3f7a75cc5896eb7ee58280564674ad6824878a9
wifidu <875147715@qq.com>
2019-10-24 14:24:30 +0800

    召回密码

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -27,5 +27,14 @@ class ResetPasswordController extends Controller
      */
     protected $redirectTo = '/';
 
-
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+    
+    protected function sendResetResponse(Request $request, $response)
+    {
+        session()->flash('success', '密码更新成功，您已成功登录！');
+        return redirect($this->redirectPath());
+    }
 }
