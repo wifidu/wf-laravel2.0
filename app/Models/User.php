bbc1e452e9984ffbae55178bb3b360aaa668fd52
wifidu <875147715@qq.com>
2019-10-29 22:19:38 +0800

    消息同志有新回复

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,27 @@ use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
+use Auth;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
-    use Notifiable,MustVerifyEmailTrait;
+    use MustVerifyEmailTrait;
+
+    use Notifiable{
+        notify as protected laravelNotify;
+    }
+    public function notify($instance)
+    {
+      if($this->id == Auth::id()){
+        return;
+      }
+
+      if(method_exists($instance,'toDatabase')){
+        $this->increment('notification_count');
+      }
+
+      $this->laravelNotify($instance);
+    }
 
     /**
      * The attributes that are mass assignable.
