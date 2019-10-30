6b86e2905e65e561e93c574507ee75177be4e3dd
wifidu <875147715@qq.com>
2019-10-30 20:22:01 +0800

    支持队列消息

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use App\Models\Reply;
 
-class TopicReplied extends Notification
+class TopicReplied extends Notification implements ShouldQueue
 {
     use Queueable;
     
@@ -31,7 +31,7 @@ class TopicReplied extends Notification
      */
     public function via($notifiable)
     {
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     /**
@@ -54,6 +54,15 @@ class TopicReplied extends Notification
           'topic_id' => $topic->id,
           'topic_title' => $topic->title,
       ];
+    }
+
+    public function toMail($notifiable)
+    {
+        $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
+
+        return (new MailMessage)
+          ->line('你的话题有回复！')
+          ->action('查看回复', $url);
     }
 
     /**
